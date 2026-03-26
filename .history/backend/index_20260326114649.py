5ed79d23ab61d8d9a7d2c6eb941ad639eb2cdf16
Sanika Patil <patilsanika704@gmail.com>
2026-03-26 12:34:51 +0530

    fix index file

--- .history/backend/index_20260326114649.py
+++ .history/backend/index_20260326114649.py
@@ -5,7 +5,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from db import get_connection
-from main import app
 import joblib
 import json
 import re
